7a58112434c15dec8fa1d3edd8a15f586f9c9149
Misha <misharexviashvili@yahoo.com>
2023-01-02 01:32:00 +0400

    Working todo input

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Input from "./components/Input";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+} from "react-native";
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [task, setTask] = useState([]);
+  const onChangeTextHandler = function (input) {
+    setInputText(input);
+  };
+  console.log(inputText);
+  const onPressHandler = function () {
+    setTask((currentInput) => {
+      return [
+        ...currentInput,
+        {
+          text: inputText,
+          id: Math.random().toString(),
+        },
+      ];
+    });
+  };
+  console.log(task)
   return (
     <View style={styles.container}>
-      <Input style={styles.input} />
-      <StatusBar style="auto" />
+      <TextInput
+        onChangeText={onChangeTextHandler}
+        style={styles.textInput}
+        placeholder="To Do"
+      />
+      <Button title="Add" color={"#5C1FCE"} onPress={onPressHandler} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
+    paddingHorizontal: 30,
     flexDirection: "row",
-    backgroundColor: "#fff",
+    justifyContent: "center",
   },
-  input: {
-    backgroundColor: "blue",
+  textInput: {
+    backgroundColor: "#777",
+    color: "#fff",
+    width: 150,
+    borderRadius: 10,
+    marginRight: 15,
+    height: 50,
   },
 });
