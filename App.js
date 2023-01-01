47e0352321514b6fa0497fb04b1a0d41c7bdc518
Misha <misharexviashvili@yahoo.com>
2023-01-02 01:59:55 +0400

    adding tasks works

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import {
   Button,
   StyleSheet,
   Text,
   TextInput,
   View,
-  Pressable,
+  ScrollView,
 } from "react-native";
 export default function App() {
   const [inputText, setInputText] = useState("");
@@ -13,7 +13,6 @@ export default function App() {
   const onChangeTextHandler = function (input) {
     setInputText(input);
   };
-  console.log(inputText);
   const onPressHandler = function () {
     setTask((currentInput) => {
       return [
@@ -24,33 +23,57 @@ export default function App() {
         },
       ];
     });
+    setInputText("");
   };
-  console.log(task)
   return (
-    <View style={styles.container}>
-      <TextInput
-        onChangeText={onChangeTextHandler}
-        style={styles.textInput}
-        placeholder="To Do"
-      />
-      <Button title="Add" color={"#5C1FCE"} onPress={onPressHandler} />
-    </View>
+    <Fragment>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={onChangeTextHandler}
+          style={styles.textInput}
+          placeholder="To Do"
+          value={inputText}
+        />
+        <Button title="Add" color={"#5C1FCE"} onPress={onPressHandler} />
+      </View>
+      <ScrollView>
+        {task
+          .map((item) => (
+            <View style={styles.taskContainer}>
+              <Text style={styles.taskText}>{item.text}</Text>
+            </View>
+          ))
+          .reverse()}
+      </ScrollView>
+    </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  inputContainer: {
     marginTop: 50,
     paddingHorizontal: 30,
     flexDirection: "row",
     justifyContent: "center",
+    marginBottom: 20,
   },
   textInput: {
-    backgroundColor: "#777",
-    color: "#fff",
+    backgroundColor: "#d4d6d5",
+    color: "#000",
     width: 150,
     borderRadius: 10,
     marginRight: 15,
     height: 50,
+  },
+  taskContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 20,
+    backgroundColor: "#a4abe0",
+    borderBottomColor: "#000",
+    borderBottomWidth: 2,
+  },
+  taskText: {
+    color: "#000",
   },
 });
